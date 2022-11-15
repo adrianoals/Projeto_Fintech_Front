77e531f90203a819bc976db1b25988d61d4f9f8e
Adriano Lima <dri.limasantos@gmail.com>
2022-11-15 14:39:53 -0300

    xx
    xx

---
+++ Projeto Fintech/Assests/Javascript/app.js
@@ -0,0 +1,62 @@
+
+
+// Função para esconder o Menu de Cadastro
+function loginMenuClick(){
+    document.getElementById("menuLogin").style.display = "";
+    document.getElementById("menuCadastro").style.display = "none";
+}
+
+// Função para esconder o Menu de Login
+function cadastroMenuClick(){
+    document.getElementById("menuLogin").style.display = "none";
+    document.getElementById("menuCadastro").style.display = "";
+}
+
+
+
+// Função para CPF 
+function mascara(i){
+   
+    var v = i.value;
+    
+    if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+       i.value = v.substring(0, v.length-1);
+       return;
+    }
+    
+    i.setAttribute("maxlength", "14");
+    if (v.length == 3 || v.length == 7) i.value += ".";
+    if (v.length == 11) i.value += "-";
+ 
+ }
+
+ // Função para DDI e DDD
+function mascaracel(i){
+   
+    var v = i.value;
+    
+    if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+       i.value = v.substring(0, v.length-1);
+       return;
+    }
+    
+    i.setAttribute("maxlength", "3");
+    
+ }
+
+ // Função para Celular 
+function celmask(i){
+   
+    var v = i.value;
+    
+    if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+       i.value = v.substring(0, v.length-1);
+       return;
+    }
+    
+    i.setAttribute("maxlength", "14");
+    if (v.length == 5) i.value += "-";
+ 
+ }
+
+
